f90891a21bdc9c4a1fb310ce47dfc2a04e049771
ejjays <christsonalloso021@gmail.com>
2025-05-07 10:02:22 +0800

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
 import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-  experimental: {
-    staleTimes: {
-      dynamic: 30,
-    },
-  },
+const nextConfig = {
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
   }
-});
+};
 
-export default nextConfig;
+export default withPWA(nextConfig);
