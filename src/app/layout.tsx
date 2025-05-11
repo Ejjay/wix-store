5a648f137d7c77cbf228a030a07d8cfc77b7efc2
ejjays <christsonalloso021@gmail.com>
2025-05-11 09:14:36 +0800

    make light mode by defauly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
       <body className={poppins.className} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem={true}
           disableTransitionOnChange
           storageKey="ej-theme"
